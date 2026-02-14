c13e05c0f5f21f2ed71a58dbdbcea55d9fcce0be
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2026-02-14 22:19:08 +0100

    imgs fileFilter added

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,8 +2,34 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const verifyToken = require('../middlewares/verifyToken');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer')
+const path = require('path');
+
+//
+const diskStorage = multer.diskStorage({
+     destination: function (req, file, cb) {
+          cb(null,'uploads')
+     },
+     filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+          const fileName = `user-${Date.now()}${ext}`;
+          cb(null, fileName);
+     }
+})
+const fileFilter = (req, file, cb) => {
+     const allowedTypes = ['.png', '.jpg', '.jpeg', '.webp'];
+     const ext = path.extname(file.originalname).toLowerCase();
+
+     if (allowedTypes.includes(ext)) {
+          cb(null, true);
+     } else {
+          cb(new Error('Only .png, .jpg, .jpeg, .webp allowed'), false);
+     }
+};
+const upload = multer({
+     storage: diskStorage,
+     fileFilter: fileFilter,
+})
 //get all users:
 router.route('/')
     .get(verifyToken,usersController.getAllUsers)
